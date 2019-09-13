77b2990a53da6849363a92a31c58cfcd391d8d79
Michael Mcmanimon <48217069+Mikelmx@users.noreply.github.com>
2019-09-13 17:38:08 -0500

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,18 @@
+'''
+Michael Mcmanimon
+
+Performing a Loop
+'''
+
+
+lower = int(input("Enter the lower bound: "))
+
+upper = int(input("Enter the upper bound: "))
+
+theSum = 0
+
+for number in range(lower,upper+1):
+    theSum =theSum +number
+    theSum
+
+    print (theSum)
